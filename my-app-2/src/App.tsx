73fae3cfb1de21618965a592a88bb17f0db8d5c4
frisqia fatmalaningsih <frifani123@gmail.com>
2024-04-25 05:11:29 +0700

    new

--- my-app-2/src/App.tsx
+++ my-app-2/src/App.tsx
@@ -3,13 +3,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import LoginPage from "./LoginPage";
 import QuotePage from "./QuotePage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LoginPage />
-        <QuotePage />
+        <Routes>
+          <Route path="/Login" Component={LoginPage}></Route>
+          <Route path="/Quote" Component={QuotePage}></Route>
+        </Routes>
       </header>
     </div>
   );
