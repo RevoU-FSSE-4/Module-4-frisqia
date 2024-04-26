ecddc783158bec6095a62efea4aa39454b70ed3a
frisqia fatmalaningsih <frifani123@gmail.com>
2024-04-27 01:13:21 +0700

    pusing

--- my-app-2/src/App.tsx
+++ my-app-2/src/App.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-// import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import HomeComponent from "./component/HomeComponent";
 import ContactComp from "./component/ContactComp";
-
+import { Field, Formik, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+// import logo from "./logo.svg";
 // import LoginPage from "./LoginPage";
 // import QuotePage from "./QuotePage";
 // import { Link, Route, Routes } from "react-router-dom";
@@ -11,12 +12,60 @@ import ContactComp from "./component/ContactComp";
 //import ExampleComp from "./example";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-9x1 text-blue-100 unerline">Hello</h1>
-      <HomeComponent />
-    </div>
+  const [fullName, setFullName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [page, setPage] = useState<string>("");
 
+  const SignupSchema = Yup.object().shape({
+    firstname: Yup.string()
+      .min(5, "Two Short!")
+      .max(10, "Two Long!")
+      .required("Required!!!"),
+    email: Yup.string().required("Required!!!"),
+  });
+  return (
+    // <div className="App">
+    //   <h1 className="text-9x1 text-blue-100 unerline">Hello</h1>
+    //   <HomeComponent />
+    //   }
+    // </div>
+
+    <div className="App">
+      <header className="App-header">
+        <h1>Sign Up</h1>
+        <div>
+          <Formik
+            initialValues={{
+              fullname: "",
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={(values, actions) => {
+              // setTimeout(() => {
+              //alert(JSON.stringify(values, null, 2));
+              //   setSubmitting(false);
+              setFullName(values.fullname);
+            }}
+          >
+            {/* {(props) => ( */}
+            <Form>
+              <label> Name :</label>
+              <Field
+                id="fullname"
+                name="fullname"
+                placeholder="input Your Name"
+              />
+              <ErrorMessage name="fullname" component="div" />
+              <Field id="email" name="email" placeholder="input Your email" />
+              <ErrorMessage name="fullname" component="div" />
+              <br />
+              <button type="submit">Submit</button>
+            </Form>
+            {/* )} */}
+          </Formik>
+          <>fullName: {fullName}</>
+        </div>
+      </header>
+    </div>
     // <div className="App">
     //   <header className="App-header">
     //     <h1>React Router</h1>
--- my-app-2/src/component/FormikButton.tsx
+++ my-app-2/src/component/FormikButton.tsx
@@ -1,1 +1,42 @@
-import React from "react";
+import { Field, Formik, Form } from "formik";
+import React, { useState } from "react";
+import "./App.css";
+
+function FormikButton() {
+  const [fullName, setFullName] = useState<string>("");
+  return (
+    <div className="App">
+      <header>
+        <h1>Sign Up</h1>
+        <div>
+          <Formik
+            initialValues={{
+              fullname: "",
+            }}
+            onSubmit={(values, actions) => {
+              // setTimeout(() => {
+              //alert(JSON.stringify(values, null, 2));
+              //   setSubmitting(false);
+              setFullName(values.fullname);
+            }}
+          >
+            {(props) => (
+              <Form>
+                <label> Name :</label>
+                <Field
+                  type="text"
+                  id="fullname"
+                  fullname="fullname"
+                  placeholder="input Your Name"
+                />
+                <button type="submit">Submit</button>
+              </Form>
+            )}
+          </Formik>
+          <>fullName: {fullName}</>
+        </div>
+      </header>
+    </div>
+  );
+}
+export default FormikButton;
